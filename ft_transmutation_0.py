bfa3b6c6c3119e84fad7d5681ba04c23e08f08b2
Arian Mostashari <53341622+ArianMost@users.noreply.github.com>
2026-05-06 16:02:17 +0200

    Implement ft_transmutation_0 function

---
+++ ft_transmutation_0.py
@@ -0,0 +1,14 @@
+import alchemy.transmutation.recipes
+
+
+def ft_transmutation_0() -> None:
+    print("=== Transmutation 0 ===")
+    print("Using file alchemy/transmutation/recipes.py directly")
+    print(
+        f"Testing lead to gold: "
+        f"{alchemy.transmutation.recipes.lead_to_gold()}"
+    )
+
+
+if __name__ == "__main__":
+    ft_transmutation_0()
